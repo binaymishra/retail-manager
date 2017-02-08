c7c66ace34557d48e76f105e5a0eced5e44d0bfd
Binay Mishra <binaymishra@gmail.com>
2017-02-08 09:18:16 +0530

    Addition @Bean component configuration.

--- src/main/java/com/manager/retail/RetailManagerApplication.java
+++ src/main/java/com/manager/retail/RetailManagerApplication.java
@@ -2,9 +2,28 @@ package com.manager.retail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.client.RestTemplate;
 
+@EnableAsync
 @SpringBootApplication
 public class RetailManagerApplication {
+	
+
+	@Bean
+	public ThreadPoolTaskExecutor taskExecutor() {
+	    ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+	    taskExecutor.setMaxPoolSize(10);
+	    taskExecutor.afterPropertiesSet();
+	    return taskExecutor;
+	}
+	
+	@Bean 
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RetailManagerApplication.class, args);
