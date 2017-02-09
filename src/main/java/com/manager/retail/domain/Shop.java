1976a09c1fbfae895df05ffc662a48138ccac8d6
Binay Mishra <binaymishra@gmail.com>
2017-02-09 10:03:16 +0530

    HashCode and equals method

--- src/main/java/com/manager/retail/domain/Shop.java
+++ src/main/java/com/manager/retail/domain/Shop.java
@@ -57,9 +57,54 @@ public class Shop {
 	public void setShopLatitude(BigDecimal shopLatitude) {
 		this.shopLatitude = shopLatitude;
 	}
+	
 
 	@Override
 	public String toString() {
 		return "Shop [shopName=" + shopName + ", shopAddress=" + shopAddress + ", shopLongitude=" + shopLongitude + ", shopLatitude=" + shopLatitude + "]";
 	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Shop other = (Shop) obj;
+		if (shopAddress == null) {
+			if (other.shopAddress != null)
+				return false;
+		} else if (!shopAddress.equals(other.shopAddress))
+			return false;
+		if (shopLatitude == null) {
+			if (other.shopLatitude != null)
+				return false;
+		} else if (!shopLatitude.equals(other.shopLatitude))
+			return false;
+		if (shopLongitude == null) {
+			if (other.shopLongitude != null)
+				return false;
+		} else if (!shopLongitude.equals(other.shopLongitude))
+			return false;
+		if (shopName == null) {
+			if (other.shopName != null)
+				return false;
+		} else if (!shopName.equals(other.shopName))
+			return false;
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((shopAddress == null) ? 0 : shopAddress.hashCode());
+		result = prime * result + ((shopLatitude == null) ? 0 : shopLatitude.hashCode());
+		result = prime * result + ((shopLongitude == null) ? 0 : shopLongitude.hashCode());
+		result = prime * result + ((shopName == null) ? 0 : shopName.hashCode());
+		return result;
+	}
+
 }
